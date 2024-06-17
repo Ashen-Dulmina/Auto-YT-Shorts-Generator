d7ba6d1982e613870dd8f59f4c2e480fc0d96a4d
unknown <ashendulminadodangoda@gmail.com>
2024-06-17 16:59:58 +0530

    Fixed functions and improved the code

--- cat_api.py
+++ cat_api.py
@@ -10,10 +10,16 @@ def getFact():
 	cat_fact = json_format_cat_api['fact']
 	return cat_fact
 
+#2024/6/16 <function select_bgm_c at 0x0000029A6EB2C4C0> eer!
+#2024/6/17 fixed above err by correcting a typo and improved the code
+
 def select_bgv_c():
-	x_factor= random.choice(os.listdir("cat_background_clips"))#prints the function
-	return x_factor
+	x = random.choice(os.listdir("cat_background_clips"))
+	process_x = f"cat_background_clips/{x}"
+	return process_x
 
 def select_bgm_c():
-	y_factor= random.choice(os.listdir("cat_bgm"))#fix this 24/6/16 done 4 the day
-	return y_factor
+	y = random.choice(os.listdir("cat_bgm"))
+	process_y = f"cat_bgm/{y}" 
+	return process_y
+
--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from ai_generated_facts import convertMarkdown, ListAIModels, generateFact, casu
 from cat_api import getFact, select_bgv_c, select_bgm_c
 from editz import EditVid
 
-EditVid(select_bgv_c, "DUMB", 20, "white", 20, select_bgm_c, "out.mp4")
-print(select_bgm_c)
-print(select_bgv_c) #prints the function fix this im done for the day
+#EditVid(select_bgv_c(), "DUMB", 20, "white", 20, select_bgm_c(), "out.mp3") #install image magic and try this
 
 #DEFAULT WINDOWS VOICE IDS
 #"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
